32311c95e775c767f0639e18065cbec5595b42e2
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-11-04 18:31:25 +0100

    #18 First test

---
+++ .github/workflows/build_latex.py
@@ -0,0 +1,24 @@
+import glob
+import subprocess
+import os
+
+repo_dir = os.environ['GITHUB_WORKSPACE']
+
+sources = glob.glob("{}/**/*.tex".format(repo_dir), recursive=True)
+
+# Do not build template
+sources_to_compile = []
+for source in sources:
+    if "template" not in source:
+        sources_to_compile.append(source)
+        print(source)
+
+build_dir = "{}/build/".format(repo_dir)
+
+if not os.path.exists(build_dir):
+    os.makedirs("{}candidatura/documenti_interni/verbali".format(build_dir))
+    os.makedirs("{}candidatura/documenti_esterni/verbali".format(build_dir))
+    os.makedirs("{}candidatura/documenti_esterni/presentazione_candidatura".format(build_dir))
+
+for source in sources_to_compile:
+    subprocess.run(["pdflatex", "-output-directory", build_dir+os.path.dirname(source), source])
