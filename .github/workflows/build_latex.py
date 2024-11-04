0dd70b78f906e3eccd105d173fca714aa2e68d4b
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2024-11-04 20:00:55 +0100

    A

--- .github/workflows/build_latex.py
+++ .github/workflows/build_latex.py
@@ -20,6 +20,8 @@ if not os.path.exists(build_dir):
     os.makedirs("{}candidatura/documenti_esterni/verbali".format(build_dir))
     os.makedirs("{}candidatura/documenti_esterni/presentazione_candidatura".format(build_dir))
 
+print(build_dir+os.path.dirname(source))
+
 for source in sources_to_compile:
     res = subprocess.run(["pdflatex", "-output-directory", build_dir+os.path.dirname(source), "-halt-on-error", source])
     if res.returncode != 0:
